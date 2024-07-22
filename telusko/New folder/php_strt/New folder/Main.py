499b586d7a55c74121deb485e799cfe244a2123e
Sanjay D <127428306+sanjay-chan@users.noreply.github.com>
2024-07-22 22:19:50 +0530

    post parameters

--- telusko/New folder/php_strt/New folder/Main.py
+++ telusko/New folder/php_strt/New folder/Main.py
@@ -6,7 +6,9 @@
 
 import sys
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import parse_qs, urlparse
 from Core.Router import Router
+import json
 
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -20,10 +22,25 @@ class RequestHandler(BaseHTTPRequestHandler):
         handler = self.find_handler(method, self.path)
         
         if handler:
+            if method == 'POST':
+                content_length = int(self.headers['Content-Length']) if 'Content-Length' in self.headers else 0
+                if content_length > 0:
+                    body = self.rfile.read(content_length).decode('utf-8')
+                    request_data = json.loads(body)  # Parse JSON data
+                else:
+                    request_data = {}      
+                # Call handler with request_data
+                response = handler(request_data)  
+            else:
+                # Extract query parameters
+                parsed_url = urlparse(self.path)
+                query_params = parse_qs(parsed_url.query)
+                # Call handler with query_params
+                response = handler(query_params) 
+                    
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            response = handler()  
             print(response)
             self.wfile.write(response.encode())  
         else:
@@ -33,12 +50,15 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(b"Not found")
     
     def find_handler(self, method, path):
+        parsed_path = urlparse(path)
         route = Router.get_routes()
+        
         for route_method, route_path, handler in route:
-            if route_method == method and route_path == path:
+            if route_method == method and parsed_path.path == route_path:
                 return handler
         
         return None
+
     
     def log_message(self, format, *args):
         # Override log_message to suppress logging to stdout
--- telusko/New folder/php_strt/New folder/Site/Helper/TaskHelper.py
+++ telusko/New folder/php_strt/New folder/Site/Helper/TaskHelper.py
@@ -1,8 +1,10 @@
 # php_strt/Site/Helper/TaskHelper.py
 
 class TaskHelper:
-    def create_task():
-        return("Task Created")
+    
+    def create_task(request_data):
+        name = request_data.get('name', [''])[0] 
+        return f"Task {name} Created" if name else "Task Created"
 
     def run_task():
         return ("Task Running")
